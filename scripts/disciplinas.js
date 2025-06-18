bf006904c4b98523f3e22fc197d827b794d4bad4
111111ricky <89875706+111111ricky@users.noreply.github.com>
2025-06-18 21:14:10 +0100

    new file:   assets/aed.jpg 	new file:   assets/analise-sistemas.jpg 	new file:   assets/bases-de-dados.jpeg 	new file:   assets/estatistica.png 	new file:   assets/iot.png 	new file:   assets/sistemasoperativos.png 	modified:   scripts/disciplinas.js

--- scripts/disciplinas.js
+++ scripts/disciplinas.js
@@ -1,10 +1,10 @@
 const Disciplinas = [
-    { id: 1, name: "Tecnologias de Internet", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/background.png" },
-    { id: 2, name: "Sistemas Operativos", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/background.png" },
-    { id: 3, name: "Bases de Dados", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/background.png" },
-    { id: 4, name: "Estatística e Probabilidades", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/background.png" },
-    { id: 5, name: "Análise de Sistemas", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/background.png" },
-    { id: 6, name: "Algoritmos e Estruturas de Dados", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/background.png" }
+    { id: 1, name: "Tecnologias de Internet", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/iot.png" },
+    { id: 2, name: "Sistemas Operativos", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/sistemasoperativos.png" },
+    { id: 3, name: "Bases de Dados", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/bases-de-dados.jpeg" },
+    { id: 4, name: "Estatística e Probabilidades", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/estatistica.png" },
+    { id: 5, name: "Análise de Sistemas", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/analise-sistemas.jpg" },
+    { id: 6, name: "Algoritmos e Estruturas de Dados", description: "UMAIA : 2024/25 : 2º Semestre", image: "/assets/aed.jpg" }
 ];
 
 const container = document.getElementById("disciplinas-container");
